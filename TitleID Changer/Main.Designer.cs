23abbb51310d8347bee37393ecc3d1aeae94d797
Gualdimar <multimeter42@gmail.com>
2016-11-08 16:03:39 +0200

    Initial commit

---
+++ TitleID Changer/Main.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace TitleID_Changer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openXex = new System.Windows.Forms.Button();
+            this.addID = new System.Windows.Forms.Button();
+            this.changeID = new System.Windows.Forms.Button();
+            this.mediaID = new System.Windows.Forms.TextBox();
+            this.titleID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openXex
+            // 
+            this.openXex.Location = new System.Drawing.Point(177, 12);
+            this.openXex.Name = "openXex";
+            this.openXex.Size = new System.Drawing.Size(91, 23);
+            this.openXex.TabIndex = 0;
+            this.openXex.Text = "Open XEX";
+            this.openXex.UseVisualStyleBackColor = true;
+            this.openXex.Click += new System.EventHandler(this.openXex_Click);
+            // 
+            // addID
+            // 
+            this.addID.Enabled = false;
+            this.addID.Location = new System.Drawing.Point(177, 42);
+            this.addID.Name = "addID";
+            this.addID.Size = new System.Drawing.Size(91, 23);
+            this.addID.TabIndex = 1;
+            this.addID.Text = "Add TitleID";
+            this.addID.UseVisualStyleBackColor = true;
+            this.addID.Click += new System.EventHandler(this.addID_Click);
+            // 
+            // changeID
+            // 
+            this.changeID.Enabled = false;
+            this.changeID.Location = new System.Drawing.Point(177, 72);
+            this.changeID.Name = "changeID";
+            this.changeID.Size = new System.Drawing.Size(91, 23);
+            this.changeID.TabIndex = 2;
+            this.changeID.Text = "Change TitleID";
+            this.changeID.UseVisualStyleBackColor = true;
+            this.changeID.Click += new System.EventHandler(this.changeID_Click);
+            // 
+            // mediaID
+            // 
+            this.mediaID.Location = new System.Drawing.Point(71, 57);
+            this.mediaID.MaxLength = 8;
+            this.mediaID.Name = "mediaID";
+            this.mediaID.Size = new System.Drawing.Size(100, 20);
+            this.mediaID.TabIndex = 4;
+            // 
+            // titleID
+            // 
+            this.titleID.Location = new System.Drawing.Point(71, 22);
+            this.titleID.MaxLength = 8;
+            this.titleID.Name = "titleID";
+            this.titleID.Size = new System.Drawing.Size(100, 20);
+            this.titleID.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "TitleID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "MediaID:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "*.xex";
+            this.openFileDialog1.Filter = "*.xex|*.xex";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 107);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mediaID);
+            this.Controls.Add(this.titleID);
+            this.Controls.Add(this.changeID);
+            this.Controls.Add(this.addID);
+            this.Controls.Add(this.openXex);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "TitleID Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openXex;
+        private System.Windows.Forms.Button addID;
+        private System.Windows.Forms.Button changeID;
+        private System.Windows.Forms.TextBox mediaID;
+        private System.Windows.Forms.TextBox titleID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ TitleID Changer/Main.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace TitleID_Changer
+{
+    public partial class Main : Form
+    {
+        private const int _sizeOffset = 0x17;
+        private const int _tidOffset = 0xC;
+        private const int _headerSize = 0x8;
+        private const int _blockSize = 0xF;
+
+        private string _xexFile;
+        private int _size;
+        private int _offset;
+        private bool _isTid = false;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void openXex_Click(object sender,EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                titleID.Clear();
+                mediaID.Clear();
+                _isTid = false;
+
+                using (var br = new BinaryReader(File.Open(openFileDialog1.FileName,FileMode.Open,FileAccess.Read,FileShare.None)))
+                {
+                    var check = br.ReadBytes(4);
+
+                    if (Encoding.ASCII.GetString(check) != "XEX2")
+                    {
+                        MessageBox.Show("Not a xex file");
+                        return;
+                    }
+
+                    br.BaseStream.Seek(_sizeOffset,SeekOrigin.Begin);
+                    _size = br.ReadByte();
+
+                    for(int i = 0; i < _size; i++)
+                    {
+                        var offset = _sizeOffset + 1 + _headerSize * i;
+                        br.BaseStream.Seek(offset,SeekOrigin.Begin);
+                        var info = br.ReadBytes(4);
+
+                        if (info[0] == 0x0 && info[1] == 0x4 && info[2] == 0x0 && info[3] == 0x6)
+                        {
+                            _isTid = true;
+
+                            _offset = Swap(BitConverter.ToUInt32(br.ReadBytes(4), 0));
+
+                            br.BaseStream.Seek(_offset,SeekOrigin.Begin);
+                            var id = br.ReadBytes(4);
+                            foreach (var byt in id)
+                                mediaID.Text += String.Format("{0:X2}",byt);
+
+                            br.BaseStream.Seek(_tidOffset-4,SeekOrigin.Current);
+                            id = br.ReadBytes(4);
+                            foreach (var byt in id)
+                                titleID.Text += String.Format("{0:X2}",byt);
+                        }
+                    }
+
+                    if (!_isTid)
+                    {
+                        MessageBox.Show("Can`t find TitleID");
+                        titleID.Text = "00000000";
+                        mediaID.Text = "00000000";
+                    }
+
+                    _xexFile = openFileDialog1.FileName;
+                    addID.Enabled = !_isTid;
+                    changeID.Enabled = _isTid;
+                }
+            }
+        }
+
+        private void addID_Click(object sender,EventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(titleID.Text,@"\A\b[0-9a-fA-F]+\b\Z") || !System.Text.RegularExpressions.Regex.IsMatch(mediaID.Text,@"\A\b[0-9a-fA-F]+\b\Z"))
+            {
+                MessageBox.Show("IDs must be in hex");
+                return;
+            }
+
+            bool add = true;
+            int tidheader;
+            int offset;
+
+            using (var br = new BinaryReader(File.Open(_xexFile,FileMode.Open,FileAccess.Read,FileShare.None)))
+            {
+                tidheader = _sizeOffset + 1 + _size * _headerSize;
+                br.BaseStream.Seek(tidheader,SeekOrigin.Begin);
+                if (BitConverter.ToUInt64(br.ReadBytes(8), 0) != 0)
+                    add = false;
+
+                br.BaseStream.Seek(_sizeOffset + 5 + (_size - 1) * _headerSize,SeekOrigin.Begin);
+                offset = Swap(BitConverter.ToUInt32(br.ReadBytes(4),0)) + _blockSize + 1;
+
+                br.BaseStream.Seek(offset,SeekOrigin.Begin);
+                var block1 = BitConverter.ToUInt64(br.ReadBytes(8), 0);
+                var block2 = BitConverter.ToUInt64(br.ReadBytes(8), 0);
+                if (block1 != 0 || block2 != 0)
+                    add = false;
+            }
+
+            if (!add)
+            {
+                MessageBox.Show("Can`t add titleid");
+                return;
+            }
+
+            using (var bw = new BinaryWriter(File.Open(_xexFile,FileMode.Open,FileAccess.Write,FileShare.None)))
+            {
+                bw.Seek(_sizeOffset,SeekOrigin.Begin);
+                bw.Write(_size + 1);
+
+                bw.Seek(tidheader,SeekOrigin.Begin);
+                byte[] buffer = { 0x0,0x4,0x0,0x6 };
+                bw.Write(buffer);
+                bw.Write(Swap((UInt32)offset));
+
+                _offset = offset;
+
+                bw.Seek(_offset,SeekOrigin.Begin);
+                bw.Write(StringToByteArray(mediaID.Text));
+                bw.Seek(_offset + _tidOffset,SeekOrigin.Begin);
+                bw.Write(StringToByteArray(titleID.Text));
+
+                _isTid = true;
+                addID.Enabled = !_isTid;
+                changeID.Enabled = _isTid;
+
+                MessageBox.Show("Done!");
+            }
+
+        }
+
+        private void changeID_Click(object sender,EventArgs e)
+        {
+            if (!System.Text.RegularExpressions.Regex.IsMatch(titleID.Text,@"\A\b[0-9a-fA-F]+\b\Z") || !System.Text.RegularExpressions.Regex.IsMatch(mediaID.Text,@"\A\b[0-9a-fA-F]+\b\Z"))
+            {
+                MessageBox.Show("IDs must be in hex");
+                return;
+            }
+
+            using (var bw = new BinaryWriter(File.Open(_xexFile,FileMode.Open,FileAccess.Write,FileShare.None)))
+            {
+                bw.Seek(_offset,SeekOrigin.Begin);
+                bw.Write(StringToByteArray(mediaID.Text));
+                bw.Seek(_offset + _tidOffset,SeekOrigin.Begin);
+                bw.Write(StringToByteArray(titleID.Text));
+
+                MessageBox.Show("Done!");
+            }
+        }
+
+        private static int Swap(UInt32 value)
+        {
+            return (int)((value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 | (value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24);
+        }
+
+        public static byte[] StringToByteArray(string hex)
+        {
+            while (hex.Length < 8)
+            {
+                hex = '0' + hex;
+            }
+
+            return Enumerable.Range(0,hex.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(hex.Substring(x,2),16))
+                             .ToArray();
+        }
+    }
+}
